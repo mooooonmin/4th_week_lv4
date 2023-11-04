5e3ab3ba4c9d4f26269cbd5eb3c7db1004e72e6a
min_moon <minhee610@gmail.com>
2023-11-05 01:06:36 +0900

    init commit

---
+++ src/test/java/com/level4/office/OfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.level4.office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
