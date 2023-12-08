1197519d47135bf03ac07b2679c94afb7eb1cfce
Minhee Moon <minhee610@gmail.com>
2023-12-08 18:09:03 +0900

    Feat: 전체 수정

--- src/main/java/com/level4/office/entity/enumType/CategoryTypeEnum.java
+++
@@ -1,7 +0,0 @@
-package com.level4.office.entity.enumType;
-
-public enum CategoryTypeEnum {
-    SPRING,
-    NODE,
-    REACT
-}
--- src/main/java/com/level4/office/entity/enumType/GenderTypeEnum.java
+++
@@ -1,6 +0,0 @@
-package com.level4.office.entity.enumType;
-
-public enum GenderTypeEnum {
-    MALE,
-    FEMALE
-}
